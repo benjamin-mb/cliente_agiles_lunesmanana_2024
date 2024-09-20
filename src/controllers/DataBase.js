ab00cff8c6fd9fb3f2608f2a8cf3fa7820e82441
Benjamin Munera <benjaminmunera0@gmail.com>
2024-09-20 07:24:31 -0500

    se crea arhivo para simular la base de datos

---
+++ src/controllers/DataBase.js
@@ -0,0 +1,28 @@
+//arreglos
+let gastos = Array(
+
+    {//objetoss
+        id: 1,
+        descripcion: "pasaje de sabaneta a cesde",
+        monto: 8000,
+        date: "2024/9/20"
+    },
+    {
+        id: 2,
+        descripcion: "pastel de queso y gaseosa",
+        monto: 10000,
+        date: "2024/9/20"   
+    },
+    {
+        id: 3,
+        descripcion: "Indriver para donde mi novia",
+        monto: 7400,
+        date: "2024/9/04"   
+    },
+    {
+        id: 3,
+        descripcion: "Indriver para donde mi novia",
+        monto: 7400,
+        date: "2024/9/04"   
+    }
+)
