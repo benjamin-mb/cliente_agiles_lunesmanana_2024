04dc34ff97678634a59bfe587fb6f3d1b28c314d
Benjamin Munera <benjaminmunera0@gmail.com>
2024-09-27 07:24:59 -0500

    se hacen filtros en el arreglo y ademas se empieza la ruta para pintar el js

---
+++ src/controllers/DataBaseSimulator.js
@@ -0,0 +1,77 @@
+//arreglos
+ export let gastos = Array(//el export es para pasra esto a otro archivo
+
+    {//objetoss
+        id: 1,
+        descripcion: "pasaje de sabaneta a cesde",
+        monto: 8000,
+        date: "2024/9/20",
+        categoria:"transporte"
+    },
+    {
+        id: 2,
+        descripcion: "pastel de queso y gaseosa",
+        monto: 10000,
+        date: "2024/9/20",   
+        categoria:"comida"
+    },
+    {
+        id: 3,
+        descripcion: "Indriver para donde mi novia",
+        monto: 7400,
+        date: "2024/9/04",
+        categoria:"transporte"   
+
+    },
+    {
+        id: 4,
+        descripcion: "Indriver para motel",
+        monto: 20000,
+        date: "2024/9/21",
+        categoria:"transporte" 
+    },
+    {
+        id: 5,
+        descripcion: "Clase de yoga",
+        monto: 100000,
+        date: "2024/9/21",   
+        categoria:"entretenimiento"
+    },
+    {
+        id: 6,
+        descripcion: "paleta de agua",
+        monto: 1500,
+        date: "2024/9/20",
+        categoria:"comida"   
+
+    },
+    {
+        id: 7,
+        descripcion: "salida a restaurante",
+        monto: 100000,
+        date: "2024/9/21",  
+        categoria:"entretenimiento" 
+    },
+    {
+        id: 8,
+        descripcion: "ida a cine",
+        monto: 55000,
+        date: "2024/9/28",  
+        categoria:"entretenimiento" 
+    },
+    {
+        id: 9,
+        descripcion: "recarga civica",
+        monto: 15500,
+        date: "2024/9/23",  
+        categoria:"transporte"
+    },
+    {
+        id: 10,
+        descripcion: "Boleta nacional-medellin",
+        monto: 89000,
+        date: "2024/10/1",  
+        categoria:"entretenimiento"
+    }
+    
+)
--- src/controllers/controladorFiltros.js
+++ src/controllers/controladorFiltros.js
@@ -1,3 +1,43 @@
-import { gastos } from "./DataBase";
+import { gastos } from "./DataBaseSimulator.js";
 
 console.log(gastos)
+
+//yo como ciente quisiera ver los gastos mayores a 100000
+
+
+//Funciones para filtrar datos en los arreglos. aqui se hace una funcion anonima y se xplica que haga un filtro a cada gasto
+let filtromonto = gastos.filter(function(gasto){
+    return gasto.monto>10000
+})
+
+let filtroMenoresde5 = gastos.filter(function(gasto){
+    return gasto.monto<5000
+})
+
+let filtrotransporte= gastos.filter(function(gasto){
+    return gasto.categoria == "transporte"
+})
+
+let filtrotroalimentacion= gastos.filter(function(gasto){
+    return gasto.categoria == "comida"
+})
+
+
+let filtroentretenimiento= gastos.filter(function(gasto){
+    return gasto.categoria == "entretenimiento"
+})
+
+//misma funcion en flecha 
+ // gastos.filter(gasto => gasto.monto>10000)
+
+ //RUTINA PARA TRAVERSING
+ //llama los elementos desde html para poder darles estilos
+ let fila=document.getElementById
+
+
+ //recorrer el arreglo que voy a pintar
+ gastos.forEach(function(gasto){
+
+
+    //RUTINA PARA ACCEDER A LA INFO
+ })
